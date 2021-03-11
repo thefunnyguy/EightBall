b79ac688e7edb615aa4ce41418e54520793f427f
thefunnyguy <73847983+thefunnyguy@users.noreply.github.com>
2021-03-11 15:40:20 -0600

    Create EightBall.js

---
+++ EightBall.js
@@ -0,0 +1,42 @@
+let userName = 'Cole';
+console.log('Hello '+ userName + '!'); console.log('Hello');
+let userQuestion = 'is bradley stinky?';
+console.log(`the user asked: ${userQuestion}`);
+let randomNumber = Math.floor(Math.random() * 8);
+let eightBall = 'Random';
+
+switch(randomNumber){
+case 0:
+eightBall= 'its certain';
+break;
+
+case 1:
+eightBall = 'its  is true';
+break;
+
+case 2:
+eightBall = 'reply is hazy'
+case 3:
+break;
+
+eightBall = 'cannot tell';
+break;
+
+case 4:
+eightBall = 'dont count on it';
+break;
+
+case 5:
+eightBall = 'No';
+break;
+
+case 6:
+eightBall = 'outlook not so good';
+break;
+
+case 7:
+eightBall = 'probably';
+break;
+}
+
+console.log(eightBall);
